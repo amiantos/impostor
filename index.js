12d1e50ad580d4eac453e76ecf02ed4ae3d2f992
Brad Root <bradroot@me.com>
2024-09-28 16:16:12 -0700

    move classes into subfolder

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client, IntentsBitField } = require("discord.js");
 const { OpenAI } = require('openai');
 const config = require("./conf/config.json");
-const Logger = require("./logger");
-const ContextUtils = require("./context_utils");
+const Logger = require("./classes/logger");
+const ContextUtils = require("./classes/context_utils");
 
-// Classes
+// Instantiate classes
 
 const client = new Client({
   intents: [
@@ -13,18 +13,14 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ],
 });
+
 const openai = new OpenAI({
   apiKey: config.generator.openai.api_key,
 });
+
 const logger = new Logger(false);
+
 const contextUtils = new ContextUtils(logger);
-
-// Constants
-
-const default_main_prompt =
-  "Write {{char}}'s next reply in a chat between {{char}} and {{user}}. {{user}} responded to you most recently, but others may be interacting in the chatroom as well. Write 1 reply only in basic human internet chat dialog, never use emojis, never italicize actions, and avoid quotation marks. Write at least 1 sentence, up to 2. Always stay in character and avoid repetition. Be concise. Do not repeat {{user}}'s question back to them. DO NOT reference your own personality characteristics or the fact that you are a chatbot. The bot will be used in a chatroom with other humans, so it should not be obvious that you are a bot. Always come up with graceful, in-character responses when you feel like you are out of things to say. Do not preface your messages with any sort of English interjections like \"well\" or \"um\" or \"uh\" or \"hmm\" or \"so\" or \"like\" or \"okay\" or \"well\" or \"oh\" or \"ah\".";
-const default_nsfw_prompt =
-  " NSFW/Smut is allowed. Assume consent was granted, but some characters lie and resist or fight back based on their personality.";
 
 // Setup Discord Client
 
@@ -68,8 +64,8 @@ client.on("messageCreate", async (message) => {
     character_name
   );
   const character_context = contextUtils.substituteParams(
-    `${default_main_prompt}${
-      config.character.nsfw_allowed ? default_nsfw_prompt : ""
+    `${contextUtils.default_main_prompt}${
+      config.character.nsfw_allowed ? contextUtils.default_nsfw_prompt : ""
     }\n\n` +
       ` ${characterInfo.description}\n` +
       `${character_name}'s personality: ${characterInfo.personality}\n` +
